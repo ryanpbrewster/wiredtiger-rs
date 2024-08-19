c768c08ad209d76746893335b71e9da09b11675c
Ryan P. Brewster <RyanPBrewster@gmail.com>
2024-08-18 21:49:09 -0400

    hello

--- build.rs
+++ build.rs
@@ -1,9 +1,19 @@
 use std::process::Command;
 
 fn main() {
-    println!("cargo:rerun-if-changed=vendor");
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed=vendor");
 
-    Command::new("tar").arg("xf").arg("vendor/3.1.0.tar.gz")
-      .output()
-      .expect("untar vendored code");
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR env var");
+
+    let output = Command::new("tar")
+        .arg("--extract")
+        .arg("--file=vendor/3.1.0.tar.gz")
+        .arg(format!("--directory={out_dir}"))
+        .arg("--strip-components=2")
+        .output()
+        .expect("untar vendored code");
+    if !output.status.success() {
+        panic!("failed to untar: {:?}", output);
+    }
 }
